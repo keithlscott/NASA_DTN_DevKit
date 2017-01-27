bfae2fcfecdb9669d4e6aa567391c1127b025b91
Keith Scott <kscott@mitre.org>
2017-01-27 13:22:40 -0500

    Removed extraneous stuff.

--- CORE_configs/3GS/foo_GS1/link/nm_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MANAGER=ipn:5.5
-
-echo 'a endpoint ipn:2.5 x' | bpadmin
-nm_agent ipn:2.5 $MANAGER &> nm_agent.out &
-
--- CORE_configs/3GS/generalSetup.sh
+++ CORE_configs/3GS/generalSetup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# NOTICE
+# This software / technical data was produced for the U.S.
+# Government under Prime Contract No. NNN12AA01C and JPL
+# Contract Number 149581, and is subject to the FAR
+# 52.227-14 (6/87) Rights in Data General.
+#
+# Approved for Public Release; Distribution Unlimited.
+# MITRE Case Number XX-YYYY
+
 export LD_LIBRARY_PATH=/usr/local/bin:$LD_LIBRARY_PATH
 
 logfile=`hostname`.log
